36ccf9fd7cc7411ab8817eee02f697ed94a14b09
Алексей Иванов <Alessio18911@gmail.com>
2019-09-28 22:17:42 +0300

    Revert "Вообще не работает ничего с файла, только с консоли"
    This reverts commit 09fe5f8086f54a37d9f2cf571c6d409e0f5dcacf.

--- js/main.js
+++ js/main.js
@@ -208,7 +208,6 @@ var pins = map.querySelector('.map__pins');
 var mainPin = pins.querySelector('.map__pin--main');
 var adForm = document.querySelector('.ad-form');
 var adFormFields = adForm.querySelectorAll('fieldset');
-var adFormAddressField = adForm.querySelector('input[name="address"]');
 
 renderPins(accomodations);
 renderAdvertisment(accomodations[0]);
