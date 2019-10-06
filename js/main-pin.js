6d3780ac3652c1540ce0ff52d65b76471b18bc76
Алексей Иванов <Alessio18911@gmail.com>
2019-10-06 22:46:04 +0300

    Доделал метку, двигается в заданном диапазоне

--- js/main-pin.js
+++ js/main-pin.js
@@ -3,15 +3,22 @@
 (function () {
   var MAIN_PIN_WIDTH = 65;
   var MAIN_PIN_HEIGHT = 82;
+  var MAIN_PIN_HALF_WIDTH = MAIN_PIN_WIDTH / 2;
+  var MAP_MAX_WIDTH = 1201;
+  var MAP_MAX_HEIGHT = 631;
+  var MAIN_PIN_X_MIN = -MAIN_PIN_HALF_WIDTH;
+  var MAIN_PIN_X_MAX = MAP_MAX_WIDTH - MAIN_PIN_HALF_WIDTH;
+  var MAIN_PIN_Y_MIN = 47;
+  var MAIN_PIN_Y_MAX = MAP_MAX_HEIGHT - MAIN_PIN_HEIGHT;
   var mainPin = document.querySelector('.map__pin--main');
   var mainPinImg = mainPin.querySelector('img');
 
-  window.form.fillAddressField(mainPin, MAIN_PIN_WIDTH, MAIN_PIN_HEIGHT, window.availability.isPageActive);
+  window.form.fillInAddressField(mainPin, MAIN_PIN_WIDTH, MAIN_PIN_HEIGHT, window.availability.isPageActive);
 
   mainPin.addEventListener('keydown', function (evt) {
     if (evt.keyCode === 13) {
       var isPageActive = !window.availability.isPageActive;
-      window.form.fillAddressField(mainPin, MAIN_PIN_WIDTH, MAIN_PIN_HEIGHT, isPageActive);
+      window.form.fillInAddressField(mainPin, MAIN_PIN_WIDTH, MAIN_PIN_HEIGHT, isPageActive);
       window.availability.activatePage(isPageActive, window.availability.dialogFields, window.map.map, window.form.adForm);
     }
   });
@@ -19,7 +26,7 @@
   mainPinImg.addEventListener('mousedown', function (evt) {
     evt.preventDefault();
     var isPageActive = !window.availability.isPageActive;
-    window.form.fillAddressField(mainPin, MAIN_PIN_WIDTH, MAIN_PIN_HEIGHT, isPageActive);
+    window.form.fillInAddressField(mainPin, MAIN_PIN_WIDTH, MAIN_PIN_HEIGHT, isPageActive);
     window.availability.activatePage(isPageActive, window.availability.dialogFields, window.map.map, window.form.adForm);
 
     var startCoords = {
@@ -40,15 +47,23 @@
         y: moveEvt.clientY
       };
 
-      window.form.fillAddressField(mainPin, MAIN_PIN_WIDTH, MAIN_PIN_HEIGHT, isPageActive);
+      var mainPinHorizontalPosition = mainPin.offsetLeft - shift.x;
+      var mainPinVerticalPosition = mainPin.offsetTop - shift.y;
 
-      mainPin.style.top = mainPin.offsetTop - shift.y + 'px';
-      mainPin.style.left = mainPin.offsetLeft - shift.x + 'px';
+      if (mainPinHorizontalPosition < MAIN_PIN_X_MAX && mainPinHorizontalPosition > MAIN_PIN_X_MIN) {
+        mainPin.style.left = mainPinHorizontalPosition + 'px';
+      }
+
+      if (mainPinVerticalPosition < MAIN_PIN_Y_MAX && mainPinVerticalPosition > MAIN_PIN_Y_MIN) {
+        mainPin.style.top = mainPinVerticalPosition + 'px';
+      }
+
+      window.form.fillInAddressField(mainPin, MAIN_PIN_WIDTH, MAIN_PIN_HEIGHT, isPageActive);
     }
 
     function onMouseUp(upEvt) {
       upEvt.preventDefault();
-      window.form.fillAddressField(mainPin, MAIN_PIN_WIDTH, MAIN_PIN_HEIGHT, isPageActive);
+      window.form.fillInAddressField(mainPin, MAIN_PIN_WIDTH, MAIN_PIN_HEIGHT, isPageActive);
 
       document.removeEventListener('mousemove', onMouseMove);
       document.removeEventListener('mouseup', onMouseUp);
