db9104ee74f302173ab7066a03b8b2330bea3ece
Алексей Иванов <Alessio18911@gmail.com>
2019-10-06 20:17:20 +0300

    Сделал ввод координат в поле Адрес в зависимости от положения главного пина

--- js/main-pin.js
+++ js/main-pin.js
@@ -2,18 +2,11 @@
 
 (function () {
   var MAIN_PIN_WIDTH = 65;
-  var MAIN_PIN_HEIGHT = 80;
+  var MAIN_PIN_HEIGHT = 82;
   var mainPin = document.querySelector('.map__pin--main');
+  var mainPinImg = mainPin.querySelector('img');
 
   window.form.fillAddressField(mainPin, MAIN_PIN_WIDTH, MAIN_PIN_HEIGHT, window.availability.isPageActive);
-
-  mainPin.addEventListener('mousedown', function (evt) {
-    if (evt.which === 1) {
-      var isPageActive = !window.availability.isPageActive;
-      window.form.fillAddressField(mainPin, MAIN_PIN_WIDTH, MAIN_PIN_HEIGHT, isPageActive);
-      window.availability.activatePage(isPageActive, window.availability.dialogFields, window.map.map, window.form.adForm);
-    }
-  });
 
   mainPin.addEventListener('keydown', function (evt) {
     if (evt.keyCode === 13) {
@@ -21,5 +14,47 @@
       window.form.fillAddressField(mainPin, MAIN_PIN_WIDTH, MAIN_PIN_HEIGHT, isPageActive);
       window.availability.activatePage(isPageActive, window.availability.dialogFields, window.map.map, window.form.adForm);
     }
+  });
+
+  mainPinImg.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+    var isPageActive = !window.availability.isPageActive;
+    window.form.fillAddressField(mainPin, MAIN_PIN_WIDTH, MAIN_PIN_HEIGHT, isPageActive);
+    window.availability.activatePage(isPageActive, window.availability.dialogFields, window.map.map, window.form.adForm);
+
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    function onMouseMove(moveEvt) {
+      moveEvt.preventDefault();
+
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      window.form.fillAddressField(mainPin, MAIN_PIN_WIDTH, MAIN_PIN_HEIGHT, isPageActive);
+
+      mainPin.style.top = mainPin.offsetTop - shift.y + 'px';
+      mainPin.style.left = mainPin.offsetLeft - shift.x + 'px';
+    }
+
+    function onMouseUp(upEvt) {
+      upEvt.preventDefault();
+      window.form.fillAddressField(mainPin, MAIN_PIN_WIDTH, MAIN_PIN_HEIGHT, isPageActive);
+
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+    }
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
   });
 })();
